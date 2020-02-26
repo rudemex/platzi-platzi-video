9546ec7404be1318bcccefb5d926bd3916471e29
Maximiliano Delgado <mdelgado@baufest.com>
2020-02-25 23:49:16 -0300

    changes

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { loginRequest } from '../actions';
-import Header from '../components/Header';
-import '../assets/styles/components/Login.scss';
-import googleIcon from '../assets/static/google-icon.png';
-import twitterIcon from '../assets/static/twitter-icon.png';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { loginRequest } from "../actions";
+import Header from "../components/Header";
+import "../assets/styles/components/Login.scss";
+import googleIcon from "../assets/static/google-icon.png";
+import twitterIcon from "../assets/static/twitter-icon.png";
 
 const Login = props => {
   const [form, setValues] = useState({
-    email: '',
+    email: ""
   });
 
   const handleInput = event => {
     setValues({
       ...form,
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
     props.loginRequest(form);
-    props.history.push('/');
-  }
+    props.history.push("/");
+  };
 
   return (
     <>
@@ -49,29 +49,34 @@ const Login = props => {
             <button className="button">Iniciar sesión</button>
             <div className="login__container--remember-me">
               <label>
-                <input type="checkbox" id="cbox1" value="first_checkbox" /> Recuérdame
+                <input type="checkbox" id="cbox1" value="first_checkbox" />{" "}
+                Recuérdame
               </label>
               <a href="/">Olvidé mi contraseña</a>
             </div>
           </form>
           <section className="login__container--social-media">
-            <div><img src={googleIcon} /> Inicia sesión con Google</div>
-            <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
+            <div>
+              <img src={googleIcon} /> Inicia sesión con Google
+            </div>
+            <div>
+              <img src={twitterIcon} /> Inicia sesión con Twitter
+            </div>
           </section>
           <p className="login__container--register">
-              No tienes ninguna cuenta {' '}
-              <Link to="/register">
-                Regístrate
-              </Link>
+            No tienes ninguna cuenta <Link to="/register">Regístrate</Link>
           </p>
         </section>
       </section>
     </>
   );
-}
-
-const mapDispatchToProps = {
-  loginRequest,
 };
 
-export default connect(null, mapDispatchToProps)(Login);
+const mapDispatchToProps = {
+  loginRequest
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Login);
